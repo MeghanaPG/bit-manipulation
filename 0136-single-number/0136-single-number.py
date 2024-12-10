90b3fe65764528fe579353abf6a62226adb03040
MeghanaPG <55728825+MeghanaPG@users.noreply.github.com>
2024-12-10 12:42:04 -0500

    Time: 3 ms (68.05%), Space: 19.2 MB (13.34%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -2,7 +2,7 @@ class Solution:
     def singleNumber(self, nums: List[int]) -> int:
 
         # Time Complexity: O(n)
-        res = 0 #n ^ 0 = n 
+        res = 0 
 
         for n in nums:
             res = n ^ res 
